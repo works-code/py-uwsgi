ec17aa623de7ba3777f19fbbed74a03208551034
HONGYOOLEE <69740508+works-code@users.noreply.github.com>
2021-07-07 17:29:09 +0900

    upload
    upload

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2019. 03. 21.
+@author : euigine.kim
+@description
+
+@histroy
+  - 최초생성.
+'''
+from application import app
+
+if __name__ == "__main__":
+    app.run()
